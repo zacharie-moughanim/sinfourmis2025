826dfa7c81c12fcc302b0760bf3de95dc995004b
Robotechnic <robotechnic.developeur@gmail.com>
2025-01-03 16:49:51 +0100

    added the dummy strategy (that doesn't work for now)

--- include/interfaces/dummy.hpp
+++ include/interfaces/dummy.hpp
@@ -7,8 +7,12 @@ public:
 	Dummy() = default;
 	~Dummy() override = default;
 	void load(std::string_view path) override;
-	reine_retour reine_activation(const fourmi_etat fourmis[], const size_t nb_fourmis,
-	                              const reine_etat *etat, const salle *salle) override;
-	fourmi_retour fourmi_activation(const fourmi_etat *fourmi, const salle *salle) override;
-
+    reine_retour reine_activation(fourmi_etat fourmis[], const size_t nb_fourmis,
+                                  const reine_etat *etat, const salle *salle) override;
+    fourmi_retour fourmi_activation(fourmi_etat *fourmi, const salle *salle) override;
+private:
+	uint32_t fourmis_count = 0;
+  	enum { PRODUCE, SEND, GATHER, FOUND_FOOD, END } state = PRODUCE;
+	bool found_food = false;
+	char path[250];
 };
