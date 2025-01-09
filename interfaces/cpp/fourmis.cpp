8300947e46163c09e64e9106edd0c1a4b9c10640
Robotechnic <robotechnic.developeur@gmail.com>
2025-01-09 21:59:29 +0100

    added the dummy example as the cpp example

--- interfaces/cpp/fourmis.cpp
+++ interfaces/cpp/fourmis.cpp
@@ -1,10 +1,21 @@
 #include "sinfourmis.h"
 
-reine_retour reine_activation(fourmi_etat fourmis[], const unsigned int nb_fourmis,
-                              const reine_etat *etat, const salle *salle) {
+reine_retour cpp_reine_activation(fourmi_etat fourmis[], const unsigned int nb_fourmis,
+                                  const reine_etat *etat, const salle *salle) {
     return {.action = REINE_PASSE, .arg = 0};
 }
 
-fourmi_retour fourmi_activation(fourmi_etat *etat, const salle *salle) {
+fourmi_retour cpp_fourmi_activation(fourmi_etat *etat, const salle *salle) {
     return {.action = FOURMI_PASSE, .arg = 0, .depose_pheromone = false, .pheromone = 0};
+}
+
+extern "C" {
+reine_retour reine_activation(fourmi_etat fourmis[], const unsigned int nb_fourmis,
+                              const reine_etat *etat, const salle *salle) {
+    return cpp_reine_activation(fourmis, nb_fourmis, etat, salle);
+}
+
+fourmi_retour fourmi_activation(fourmi_etat *etat, const salle *salle) {
+    return cpp_fourmi_activation(etat, salle);
+}
 }
