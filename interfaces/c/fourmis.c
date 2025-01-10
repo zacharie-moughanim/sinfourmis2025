c3bf2559c8e58fb105726e0dc0e45452d211b3a2
Robotechnic <robotechnic.developeur@gmail.com>
2025-01-10 19:07:49 +0100

    added c example

---
+++ interfaces/c/fourmis.c
@@ -0,0 +1,10 @@
+#include "sinfourmis.h"
+
+reine_retour reine_activation(fourmi_etat fourmis[], const unsigned int nb_fourmis,
+                                  const reine_etat *etat, const salle *salle) {
+    return {.action = REINE_PASSE, .arg = 0};
+}
+
+fourmi_retour fourmi_activation(fourmi_etat *etat, const salle *salle) {
+    return {.action = FOURMI_PASSE, .arg = 0, .depose_pheromone = false, .pheromone = 0};
+}
---
+++ interfaces/examples/c/fourmis.c
@@ -0,0 +1,33 @@
+#include "sinfourmis.h"
+#include "stdio.h"
+
+reine_retour reine_activation(fourmi_etat fourmis[], const unsigned int nb_fourmis,
+                                  const reine_etat *etat, const salle *salle) {
+    (void)etat;
+    (void)salle;
+    static int nb_sent = 0;
+    if (nb_sent > 2) {
+        return (reine_retour){.action = REINE_PASSE, .arg = 0};
+    }
+    if (nb_fourmis < 1) {
+        printf("Création d'une fourmis\n");
+        return (reine_retour){.action = CREER_FOURMI, .arg = 1};
+    }
+    for (int i = 0; i < 256; i++) {
+        fourmis[0].memoire[i] = (char)i;
+    }
+    printf("Envoi d'une fourmi\n");
+    nb_sent += 1;
+    return (reine_retour){.action = ENVOYER_FOURMI, .arg = 1};
+}
+
+fourmi_retour fourmi_activation(fourmi_etat *etat, const salle *salle) {
+    (void)salle;
+    etat->memoire[0] += 1;
+    if (etat->memoire[0] == 1) {
+        printf("Fourmi: Je suis une fourmi\n");
+    } else {
+        printf("Fourmi: Je suis une fourmi et je sais compter\n");
+    }
+    return (fourmi_retour){.action = FOURMI_PASSE, .arg = 0, .depose_pheromone = false, .pheromone = 0};
+}
