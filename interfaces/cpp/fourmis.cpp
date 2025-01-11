01d21cbf9349d29cb8f8b23c28667e9bb096c3a3
ZachMGH <zachariemoughanim@gmail.com>
2025-01-11 14:16:09 +0100

    Implement memory access functions

--- interfaces/cpp/fourmis.cpp
+++ interfaces/cpp/fourmis.cpp
@@ -1,6 +1,8 @@
 #include "sinfourmis.h"
 #include <iostream>
 
+#define N_MEM_MOVES 85
+
 typedef enum type_ouvrieres {
     BAT_ATTAQUE, // 0
     BAT_EXPLORA, // 1
@@ -10,7 +12,10 @@ typedef enum type_ouvrieres {
 } type_ouvrieres_e;
 
 type_ouvrieres_e int_to_type_fourmi(char n) {
-    switch(n) {
+    unsigned char mask = (unsigned char)224;
+    unsigned char int_type = ((unsigned char)n) & mask;
+    int_type = int_type >> 5;
+    switch(int_type) {
         case 0 :
             return BAT_ATTAQUE;
         break;
@@ -35,17 +40,81 @@ type_ouvrieres_e int_to_type_fourmi(char n) {
 
 typedef struct infourmis {
     type_ouvrieres_e type_fourmi;
-    char last_moves[85];
+    char last_moves[N_MEM_MOVES];
     int i_moves;
 } infourmis_t;
+
+char get_half(int i, int offset, int mem[256]) {
+    int i_tab = i/2;
+    int half = i%2;
+    unsigned char res = (unsigned char)mem[i_tab];
+    unsigned char mask  = 15;
+    if(half == 0) {
+        res = res >> 4;
+        res = res & mask;
+    } else {
+        res = res & mask;
+    }
+    return res;
+}
+
+void set_half(int i, char value, int mem[256]) {
+    int i_tab = i/2;
+    int half = i%2;
+    unsigned char res = (unsigned char)mem[i_tab];
+    
+    if(half == 0) {
+        unsigned char mask  = 15;
+        res = res & mask;
+        res = res ^ (value << 4);
+    } else {
+        unsigned char mask  = 240;
+        res = res & mask;
+        res = res ^ value;
+    }
+    mem[i_tab] = res;
+}
+
+char get_move(int i_move, char mem[256]) {
+    int ind_start = i_move*6+4; // 3 bits for types, 1 of offset and 6 times the number of previous moves
+    int d = ind_start/8;
+    int r = ind_start%8;
+    if(r < 3) {
+        // All move is within a char
+        unsigned char cur = (unsigned char)mem[d];
+        cur = cur >> (2-r);
+        unsigned char mask = (char)63;
+        cur = cur & mask;
+        return cur;
+    } else {
+        // The move is between two chars
+        unsigned char half_big = (unsigned char)mem[d];
+        unsigned char half_lil = (unsigned char)mem[d+1];
+        unsigned char mask_big = (1 << (8-r)) - 1;
+        int n = r-2;
+        unsigned char mask_lil = ((1 << n) - 1) << (8-n);
+        half_big = half_big & mask_big;
+        half_lil = half_lil & mask_lil;
+        unsigned char res;
+        res = (half_big << (r-2)) + (half_lil >> n);
+        return res;
+    }
+}
+
+char get_i_move(char mem[256]) {
+
+}
 
 infourmis_t info_mem_ouvriere(char mem[256]) {
     infourmis_t res;
     // Récupère le type de fourmis
-
+    res.type_fourmi = int_to_type_fourmi(mem[0]);
     // Récupère les mouvements
-
+    for(int i = 0; i < N_MEM_MOVES; ++i) {
+        res.last_moves[i] = get_move(i, mem);
+    }
     // Récupère l'indice de mouvements
+    res.i_moves = get_i_move(mem);
     /*
     switch(type_fourmi) {
         case BAT_ATTAQUE :
@@ -59,6 +128,7 @@ infourmis_t info_mem_ouvriere(char mem[256]) {
         case BAT_NOU_CRS :
         break;
     }*/
+    return res;
 }
 
 reine_retour cpp_reine_activation(fourmi_etat fourmis[], const unsigned int nb_fourmis,
